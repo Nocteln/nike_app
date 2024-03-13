087aa6a907d418f7b48ee9be105d7f00ac3254b6
Nocteln <eliottmieze@gmail.com>
2024-03-13 17:05:07 +0100

    burger menu

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -1,15 +1,20 @@
 import { headerLogo } from "../assets/images";
-import { hamburger } from "../assets/icons";
+import { hamburger, hamburgerDark } from "../assets/icons";
 import { navLinks } from "../constants";
+import { useState } from "react";
 
 function Nav({ darkMode, onDarkMode }) {
+  const [menu, setMenu] = useState(false);
+
   return (
     <header className="padding-x py-2 absolute w-full sticky top-0 z-20 bg-white dark:bg-slate-900">
       <nav className="flex justify-between items-center max-container">
         <a href="/">
           <img src={headerLogo} alt="Logo" width={130} height={29} />
         </a>
-        <ul className="flex-1 flex justify-center items-center gap-16 max-lg:hidden">
+        <ul className="flex-1 flex justify-center items-center gap-16 max-lg:flex-col max-lg:gap-5 max-lg:hidden menu">
+          {" "}
+          {/*  */}
           {navLinks.map((link) => (
             <li key={link.label}>
               <a
@@ -32,8 +37,16 @@ function Nav({ darkMode, onDarkMode }) {
             </button>
           </li>
         </ul>
-        <div className="hidden max-lg:block">
-          <img src={hamburger} alt="hamburger" width={25} height={25} />
+        <div className="hidden max-lg:block fixed right-5 top-5">
+          <img
+            src={!darkMode ? hamburger : hamburgerDark}
+            alt="hamburger"
+            width={25}
+            height={25}
+            onClick={() =>
+              document.querySelector(".menu").classList.toggle("max-lg:hidden")
+            }
+          />
         </div>
       </nav>
     </header>
